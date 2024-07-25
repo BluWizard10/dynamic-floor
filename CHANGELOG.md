8247c8045291c8ea99639cd9fd0de8813c5f4501
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2024-07-24 20:12:53 -0700

    Added Editor Script to easily add the Prefab to your Avatar.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+# v0.3.0-beta
+- Added Editor Script that will install the Prefab automatically.
+  - Simply select your Avatar in the Scene, then activate the script from the Unity Menu via `Tools -> BluWizard LABS -> Add Dynamic Floor Collider to Avatar`.
+  - You can also do the same thing by `Right-Clicking` on your Avatar and by clicking `BluWizard LABS -> Add Dynamic Floor Collider to Avatar` in the context menu.
+
+# v0.2.1-beta
+- Fixed broken Source on Prefab due to breaking changes on SDK `3.6.2-constraints.3` version.
+- Bumped minimum version requirement of `VRChat SDK - Avatars` to `3.6.2-constraints.3`.
+
 # v0.2.0-beta
 - Added Toggle for the Floor Collider, located in the Expressions Menu as `Blu's Add-ons/Floor Collider`.
 - Removed the old `World` Object as a Source for the World Constraint, as it no longer needs a Source for the World Constraint to work as of SDK `3.6.2-constraints.2` version. [Feedback Post](https://feedback.vrchat.com/open-beta/p/1471-freeze-to-world-doesnt-work-if-parent-constraint-has-0-sources)
---
+++ Editor/AddDynamicFloorCollider.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEditor;
+using VRC.SDK3.Avatars.Components;
+
+public class AddDynamicFloorCollider : EditorWindow
+{
+    // Define the GUID of the prefab in the Packages directory
+    private static string prefabGUID = "b4d62de2cf5a8c748a002e2409e9239a";
+
+    [MenuItem("Tools/BluWizard LABS/Add Dynamic Floor Collider to Avatar")]
+    public static void AddColliderToAvatarMenu()
+    {
+        AddColliderToAvatar();
+    }
+
+    [MenuItem("GameObject/BluWizard LABS/Add Dynamic Floor Collider to Avatar", false, 10)]
+    public static void AddColliderToAvatarContextMenu(MenuCommand menuCommand)
+    {
+        AddColliderToAvatar();
+    }
+
+    public static void AddColliderToAvatar()
+    {
+        // Load the prefab using the GUID
+        string prefabPath = AssetDatabase.GUIDToAssetPath(prefabGUID);
+        GameObject floorColliderPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+        if (floorColliderPrefab == null)
+        {
+            EditorUtility.DisplayDialog("This wasn't supposed to happen...", "Could not find the Prefab with the specified GUID. Please inform BluWizard of this issue and show them this message. Missing GUID ID: " + prefabGUID, "OK");
+            return;
+        }
+
+        // Get the selected game object in the editor
+        GameObject selectedObject = Selection.activeGameObject;
+
+        if (selectedObject == null || selectedObject.GetComponent<VRCAvatarDescriptor>() == null)
+        {
+            EditorUtility.DisplayDialog("Error", "Please first select a VRChat Avatar in the current Scene, then try again. Make sure it has the VRC Avatar Descriptor Component on it.", "OK");
+            return;
+        }
+
+        // Check if the Floor Collider was already added to the selected Avatar
+        foreach (Transform child in selectedObject.transform)
+        {
+            if (child.name == floorColliderPrefab.name)
+            {
+                EditorUtility.DisplayDialog ("Already Added!", "The Dynamic VRC Floor Collider has already been added to this Avatar. If you need to reinstall it, first delete the Dynamic VRC Floor Prefab from the Avatar, then run this script again.", "OK");
+                return;
+            }
+        }
+
+        // Instantiate the prefab as a child of the selected avatar
+        GameObject instantiatedPrefab = (GameObject)PrefabUtility.InstantiatePrefab(floorColliderPrefab);
+        instantiatedPrefab.transform.SetParent(selectedObject.transform, false);
+        instantiatedPrefab.transform.localPosition = Vector3.zero;
+
+        // Record the changes in the Unity Editor
+        Undo.RegisterCreatedObjectUndo(instantiatedPrefab, "Add Floor Collider");
+        EditorUtility.SetDirty(selectedObject);
+
+        Debug.Log("Successfully added Dynamic VRC Floor to your Avatar! Don't forget to re-assign your PhysBones, such as your Tail, to use the newly added Floor Collider instead.");
+
+        // Select the newly added Prefab in the Hierarchy
+        Selection.activeObject = instantiatedPrefab;
+
+        // Ping the newly added Prefab in the Hierarchy
+        EditorGUIUtility.PingObject(instantiatedPrefab);
+    }
+
+    [MenuItem("GameObject/BluWizard LABS/Add Dynamic Floor Collider to Avatar", true)]
+    public static bool ValidateAddColliderToAvatarContextMenu()
+    {
+        GameObject selectedObject = Selection.activeGameObject;
+        return selectedObject != null && selectedObject.GetComponent<VRCAvatarDescriptor>() != null;
+    }
+}
